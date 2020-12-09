c4df5e5d5f236fcca20261504ce4be4a45a0c6fc
JakeStopford <63928890+JakeStopford@users.noreply.github.com>
2020-12-09 14:45:32 +0000

    Add files via upload

---
+++ DahlKata/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DahlKata
+{
+    public class Calculator
+    {
+        public double Checkout(List<Book> basket, double totalPrice)
+        {
+            double totalBookPrice = 0;
+            var uniqueBooks = basket.Distinct().ToList();
+
+            double discount = getTotalPrice(uniqueBooks.Count);
+
+            foreach (var book in uniqueBooks)
+            {
+                totalBookPrice += book.price;
+                basket.Remove(book);
+            }
+
+            totalPrice += totalBookPrice - (totalBookPrice * discount);
+
+            if (basket.Count != 0)
+            {
+                totalPrice = Checkout(basket, totalPrice);
+            }
+
+            return totalPrice;
+        }
+
+        private double getTotalPrice(int uniqueBooks)
+        {
+            DiscountsDictonary discounts = new DiscountsDictonary();
+
+            double discountValue = discounts.discounts[uniqueBooks];
+
+            return discountValue;
+        }
+    }
+}
---
+++ DahlKata/DiscountsDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DahlKata
+{
+    public class DiscountsDictonary
+    {
+        public Dictionary<int, double> discounts = new Dictionary<int, double>
+        {
+            {1, 0},
+            {2, 0.05},
+            {3, 0.10},
+            {4, 0.20},
+            {5, 0.25}
+        };
+    }
+}
